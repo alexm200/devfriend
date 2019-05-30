eeec24422383b848df228502ab18d34360c2324d
alexm200 <alexm200@hotmail.com>
2019-05-30 12:57:55 +0300

    commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 
-    apiUrl : process.env.REACT_APP_ENV === 'production' ? "http://devfriendapi.herokuapp.com/graphql" : "http://localhost:4000/graphql"
+    apiUrl : process.env.REACT_APP_ENV === 'production' ? "https://devfriendapi.herokuapp.com/graphql" : "http://localhost:4000/graphql"
     
 }
 
